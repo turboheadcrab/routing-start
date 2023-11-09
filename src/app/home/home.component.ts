f2260f37b442afb432ab5f88054259e9851bcc29
Vladimir Apraksin <vapraksi@utk.edu>
2023-11-09 13:03:30 -0500

    Navigating Programmatically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent {
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  onLoadServers() {
+    // complex calculation
+    this.router.navigate(['/servers']);
+  }
 }
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
   user: { id: number; name: string };
 
   constructor() {}
-
-  ngOnInit() {}
 }
