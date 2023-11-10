0edafc424c55d7e666bef8d8146770c174482880
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2023-11-09 23:22:03 -0500

    Passing Query Parameters and Fragments

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,8 +9,11 @@ import { Router } from '@angular/router';
 export class HomeComponent {
   constructor(private router: Router) {}
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     // complex calculation
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {
+      queryParams: { allowEdit: '1' },
+      fragment: 'loading',
+    });
   }
 }
